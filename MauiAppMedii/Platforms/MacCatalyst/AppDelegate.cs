1412603bec2b40a0e8aa8334b755d445227e1692
Rares <cristirares@yahoo.com>
2024-01-14 16:27:08 +0200

    Add project files.

---
+++ MauiAppMedii/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiAppMedii
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
